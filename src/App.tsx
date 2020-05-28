1c66db1424eea877ce9be41cf646ee100218735f
y1s <y9127@icloud.com>
2020-05-27 20:03:03 -0400

    installed react router

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { AppContextProvider } from "./context";
 import WordDialog from "./components/WordDialog";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 import LoginPage from "./components/LoginPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -29,18 +30,26 @@ export default function App() {
   const classes = useStyles();
 
   return (
-    <AppContextProvider>
-      <div className={classes.root}>
-        <Topbar />
-        <Container className={classes.container} maxWidth="md">
-          <Grid className={classes.grid} container spacing={0}>
-            <SideMenu />
-            <MainSection />
-          </Grid>
-        </Container>
-        {/* <LoginPage /> */}
-      </div>
-      <WordDialog />
-    </AppContextProvider>
+    <Router>
+      <AppContextProvider>
+        <div className={classes.root}>
+          <Topbar />
+          <Switch>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/">
+              <Container className={classes.container} maxWidth="md">
+                <Grid className={classes.grid} container spacing={0}>
+                  <SideMenu />
+                  <MainSection />
+                </Grid>
+              </Container>
+            </Route>
+          </Switch>
+        </div>
+        <WordDialog />
+      </AppContextProvider>
+    </Router>
   );
 }
