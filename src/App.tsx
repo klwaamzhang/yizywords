31c7a3a63c06f42fa88c69d491f7346045878029
Yizhi Zhang <36008792+klwaamzhang@users.noreply.github.com>
2020-07-23 13:05:01 -0400

    remove layout component and put the app with routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import { store } from "./store/reduxStore";
-import Layout from "./Layout";
+import Topbar from "./components/Topbar";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import LoginPage from "./components/LoginPage";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Content from "./components/Content/Content";
+import { useSelector } from "react-redux";
+import { RootState } from "./reducers";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      backgroundColor: "#f1f1f1",
+    },
+  })
+);
 
 export default function App() {
+  const classes = useStyles();
+
+  const loggedIn = useSelector((state: RootState) => state.app.loggedIn);
+
   return (
-    <Provider store={store}>
-      <Router>
-        <Layout />
-      </Router>
-    </Provider>
+    <div className={classes.root}>
+      <Topbar />
+
+      <Switch>
+        <Route path="/login">
+          {loggedIn ? <Redirect to="/Inbox" /> : <LoginPage />}
+        </Route>
+        <Route path="/:filterName">
+          <Content />
+        </Route>
+
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        {/* <Route path="*">
+              <Redirect to="/" />
+            </Route> */}
+      </Switch>
+    </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { store } from "./store/reduxStore";
 
 ReactDOM.render(
   // <React.StrictMode>
-  <App />,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   // </React.StrictMode>,
   document.getElementById("root")
 );
