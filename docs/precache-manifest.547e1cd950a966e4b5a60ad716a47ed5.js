c1a4e0bad88143c71bcaab7de04d690687ed6a0f
Yizhi Zhang <36008792+klwaamzhang@users.noreply.github.com>
2020-08-15 16:02:28 -0400

    push docs folder

---
+++ docs/precache-manifest.547e1cd950a966e4b5a60ad716a47ed5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10efec90a769a0f59170c9ee1b37b1f6",
+    "url": "/yizywords/index.html"
+  },
+  {
+    "revision": "e334e91436f9dfbe6ff7",
+    "url": "/yizywords/static/js/2.7636b621.chunk.js"
+  },
+  {
+    "revision": "1d345e9d9258a7627c67c47eea362074",
+    "url": "/yizywords/static/js/2.7636b621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e273c6f58e2694485eca",
+    "url": "/yizywords/static/js/main.d81e4c26.chunk.js"
+  },
+  {
+    "revision": "7660338df199fb04094c",
+    "url": "/yizywords/static/js/runtime-main.fee8e61b.js"
+  }
+]);
